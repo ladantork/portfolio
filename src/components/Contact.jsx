57fb16550df796a18e6384b7e729fc58f26ccc78
Ladan Torkzaban <ladantorkzaban0@gmail.com>
2024-06-21 13:08:13 +0100

    pdf.cv added, link of opening cv works, email enveloped added and works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
 
 function Contact(){
   return (
     <div>
-      <h1>Contact Me</h1>
-      <p>This is the contact page.</p>
+      <h1>Contact</h1>
+      <p>You can find all my contact details in my CV, which is available for download <a href="https://docs.google.com/document/d/177J-pyzJTHiKS-hGjSrgEFrWuuRhTPCKGpd3gjmg4ao/edit?usp=sharing"target="_blank" rel="noopener noreferrer" className="cv-link">here</a>.</p>
+      <p>Alternatively, you can reach out to me through any of the contact methods listed below:</p>
+      <ul className="contact-list">
+        <li>
+        <a href="mailto:ladantorkzaban0@gmail.com" className="contact-link">
+        <FontAwesomeIcon icon={faEnvelope} size="2x" className="fa-icon" /> 
+        
+        </a>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 function Home(){
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page.</p>
+      <h1>Ladan Torkzaban</h1>
+      <p>Junior Software Developer</p>
+      <p>Currently working as an intern in web development, and I am actively seeking a developer role.</p>
     </div>
   );
 };
