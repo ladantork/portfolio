5fba54989aff77a274f6fbf5bcb7035e21b592c6
Ladan Torkzaban <ladantorkzaban0@gmail.com>
2024-07-19 10:48:12 +0100

    In contacts all clicable icons are on same line now

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,25 +14,20 @@ function Contact(){
         <li>
         <a href="mailto:ladantorkzaban0@gmail.com" className="contact-link">
         <FontAwesomeIcon icon={faEnvelope} size="2x" className="fa-icon" /> {/* EMAIL Icon */}
-        
         </a>
-        </li>
-        <li>
+       
           <a href="sms:00447415965542" className="contact-link">
             <FontAwesomeIcon icon={faSms} size="2x" className="fa-icon" /> {/* SMS Icon */}
           </a>
-        </li>
-        <li>
+       
           <a href="https://wa.me/447415965542" className="contact-link" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faWhatsapp} size="2x" className="fa-icon" /> {/* WhatsApp Icon */}
           </a>
-        </li>
-        <li>
+      
           <a href="https://www.linkedin.com/in/ladan-torkzaban-97867259/" className="contact-link" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} size="2x" className="fa-icon" /> {/* LinkedIn Icon */}
           </a>
-        </li>
-        <li>
+     
           <a href="https://github.com/ladantork" className="contact-link" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} size="2x" className="fa-icon" /> {/* GitHub Icon */}
           </a>
