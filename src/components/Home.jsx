eebf219a53bc3cd7bac47a028a481334cff9c917
Ladan Torkzaban <ladantorkzaban0@gmail.com>
2024-07-19 11:00:18 +0100

    Home page content updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home(){
     <div>
       <h1>Ladan Torkzaban</h1>
       <p>Junior Software Developer</p>
-      <p>Currently working as an intern in web development, and I am actively seeking a developer role.</p>
+      <p>Currently working as an intern in web development at 2Toucans Marketing Agency and volunteering in Scottish Tech Army, and I am actively seeking a Software developer role.</p>
     </div>
   );
 };
